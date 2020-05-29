8c8f2728eeb42ec44632e605287d408e109b192e
Stratos <hello@bomburo.com>
2020-05-29 16:10:25 +0200

    [Script] Imported selenium and added click button function

--- get-soaps.py
+++ get-soaps.py
@@ -4,22 +4,55 @@ import requests
 from bs4 import BeautifulSoup as bs
 import os
 
+#------------------------------------
+# Selenium import
+#------------------------------------
+
+from selenium import webdriver
+import time
+
+delay=5
+
 # website with soap images
 url = 'http://perpetualsoapiness.webflow.io/'
 
-# download page for parsing
-page = requests.get(url)
-soup = bs(page.text, 'html.parser')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--test-type")
+options.binary_location = "/usr/bin/chromium"
+
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# Now you can start using Selenium
+
+#------------------------------------
+#------------------------------------
+
+
+
+browser.get(url)
+
+# wait for site to load
+time.sleep(delay)
+
+# click download button
+python_button = browser.find_elements_by_xpath("//input[@id='screenshot']")[0]
+python_button.click()
 
 # locate all elements with image tag
 image_tags = soup.findAll('img')
 
-# create directory for  images
-if not os.path.exists('soaps'):
-    os.makedirs('soaps')
+# create directory for model images
+if not os.path.exists('models'):
+    os.makedirs('models')
 
 # move to new directory
-os.chdir('soaps')
+os.chdir('models')
 
 # image file name variable
 x = 0
@@ -30,7 +63,7 @@ for image in image_tags:
         url = image['src']
         response = requests.get(url)
         if response.status_code == 200:
-            with open('soap-' + str(x) + '.jpg', 'wb') as f:
+            with open('model-' + str(x) + '.jpg', 'wb') as f:
                 f.write(requests.get(url).content)
                 f.close()
                 x += 1
