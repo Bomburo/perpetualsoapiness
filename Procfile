a5ec3d883ac694e051f35ad9103d7e88846d4100
Stratos <hello@bomburo.com>
2020-06-01 15:16:59 +0200

    [Script] Moved Proc, Requirements and get-soaps in root folder

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python get-soaps.py
--- get-soaps.py
+++ get-soaps.py
@@ -1,12 +1,7 @@
 # soap scraping for perpetualsoap bot
 
-import os
-
-#------------------------------------
-# Selenium import
-#------------------------------------
-
 from selenium import webdriver
+import os
 import time
 
 delay=5
@@ -16,15 +11,10 @@ url = 'http://perpetualsoapiness.webflow.io/'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--test-type")
-options.binary_location = "/usr/bin/chromium"
-
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Now you can start using Selenium
 
@@ -33,9 +23,4 @@ browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), 
 
 browser.get(url)
 
-# wait for site to load
-time.sleep(delay)
-
-# click download button
-python_button = browser.find_elements_by_xpath("//input[@id='screenshot']")[0]
-python_button.click()
+print(browser.page_source)
