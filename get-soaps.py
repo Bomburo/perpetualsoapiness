49a19e1d3ee2434583771c36f64fd3701a0ff723
Stratos <hello@bomburo.com>
2020-06-03 11:13:26 +0200

    Removed browser dimensions in case this causes me to exceed my memory quota

--- get-soaps.py
+++ get-soaps.py
@@ -26,7 +26,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--window-size=2560,1440")
+# chrome_options.add_argument("--window-size=2560,1440")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Now you can start using Selenium
